6f992b5be827008c9c7423019bc1bd269d746744
isrkan <111886585+isrkan@users.noreply.github.com>
2023-05-18 21:16:16 +0200

    Improving clarity and readability

--- python_files/identifying_competitors.py
+++ python_files/identifying_competitors.py
@@ -17,31 +17,31 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     # Import the store data
     Store_data = pd.read_csv('Store_data_git.csv')
     Store_data = Store_data[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
-
+    # Merge with Subchain names
     Subchain_names = pd.read_excel('SubChainNameEnglish.xlsx')
     Store_data = Store_data.merge(Subchain_names, on=['SubChainID','SubChainName'], how='left')
     Store_data['SubChainName'] = Store_data['EnglishName']
     Store_data = Store_data.drop(columns=['EnglishName'])
 
-    # save the store and chain ids
+    # Save the store and chain IDs
     Store_input_data = Store_data[(Store_data['SubChainName']==SubChainName) & (Store_data['StoreName']==StoreName)]
 
     # Import the price data in one category and convert it to wide data
-    df = pd.read_parquet(str(category+'.parquet'))
-    df = df.set_index(['category','ProductDescription','StoreID'])
+    category_df = pd.read_parquet(str(category+'.parquet'))
+    category_df = category_df.set_index(['category','ProductDescription','StoreID'])
 
     # Filter observations with more than 25% nan values
-    df_filtered = df.dropna(thresh=int(0.75*731), axis=0)
+    filtered_df = category_df.dropna(thresh=int(0.75*731), axis=0)
 
     # Interpolation - linear
-    lfill_df = df_filtered.T.interpolate(method='linear').T
+    linear_fill_df = filtered_df.T.interpolate(method='linear').T
     # Imputation - first NOCB and then LOCB
-    nlfill_df = lfill_df.T.fillna(method='bfill').T
-    lnlfill_df = nlfill_df.T.fillna(method='ffill').T
+    no_cb_fill_df = linear_fill_df.T.fillna(method='bfill').T
+    linear_no_cb_fill_df = no_cb_fill_df.T.fillna(method='ffill').T
 
-    ### choose one item and filter the data
-    lnlfill_df.reset_index(inplace=True)
-    product_df = lnlfill_df[lnlfill_df['ProductDescription'] == product_description]
+    ### Choose one item and filter the data
+    linear_no_cb_fill_df.reset_index(inplace=True)
+    product_df = linear_no_cb_fill_df[linear_no_cb_fill_df['ProductDescription'] == product_description]
     # Merge with store data
     product_df['StoreID'] = product_df['StoreID'].astype('int64')
     product_df = pd.merge(product_df, Store_data, how="left", left_on="StoreID", right_on="StoreID")
@@ -68,15 +68,15 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     ###### PCA #####
     ### Price movement ###
     pca = PCA()
-    principalComponents = pca.fit_transform(price_movement_df)
-    price_movement_principal_df = pd.DataFrame(data = principalComponents)
+    price_movement_principal_components  = pca.fit_transform(price_movement_df)
+    price_movement_principal_df = pd.DataFrame(data = price_movement_principal_components)
     price_movement_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
     price_movement_principal_pca = price_movement_principal_df.copy()
 
     ### Price level ###
     pca = PCA()
-    principalComponents = pca.fit_transform(price_level_df)
-    price_level_principal_df = pd.DataFrame(data = principalComponents)
+    price_level_principal_components  = pca.fit_transform(price_level_df)
+    price_level_principal_df = pd.DataFrame(data = price_level_principal_components)
     price_level_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
     price_level_principal_pca = price_level_principal_df.copy()
 
@@ -97,16 +97,17 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
 
     # Data preparation for clustering 
 
-    # PCA
+    # Combine PCA components for price movement and price level
     pca_components_df = pd.concat([price_movement_principal_pca.iloc[:, 0:2],price_level_principal_pca.iloc[:, 0:2]], axis=1)
     pca_components_df.columns = ['P_M_1','P_M_2','P_L_1','P_L_2']
+    # Add relevant columns
     pca_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
     pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName'], inplace=True)
 
     # Filter out stores from the same chain
     pca_components_df = pca_components_df.reset_index()
-    store_pca_components_df = pca_components_df[(pca_components_df['SubChainName']==SubChainName) & (pca_components_df['StoreName']==StoreName)]
-    other_chains_pca_components_df = pca_components_df[pca_components_df['ChainName']!=store_pca_components_df['ChainName'].iloc[0]]
+    store_pca_components_df = pca_components_df[(pca_components_df['StoreID']==Store_input_data['StoreID'].iloc[0])]
+    other_chains_pca_components_df = pca_components_df[pca_components_df['ChainID']!=Store_input_data['ChainID'].iloc[0]]
     pca_components_df = pd.concat([store_pca_components_df,other_chains_pca_components_df])
     pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName'], inplace=True)
 
@@ -115,10 +116,8 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     birch.fit(pca_components_df)
     # Get the cluster labels
     pca_birch_labels = birch.labels_
-    # Number of clusters
-    cluster_num = len(set(pca_birch_labels)) - (1 if -1 in pca_birch_labels else 0)
 
-    # BIRCH on PCA
+    # Plot BIRCH clusters on PCA components
     plt.scatter(pca_components_df['P_M_1'], pca_components_df['P_L_1'], c=pca_birch_labels)
     plt.xlabel('First Principal Component')
     plt.ylabel('Second Principal Component')
@@ -129,17 +128,18 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     def euclidean_distance(row1, row2):
         return np.sqrt(np.sum((row1 - row2)**2))
 
+    # Assign labels and filter data
     pca_components_df['pca_birch_labels'] = pca_birch_labels
     pca_components_df = pca_components_df.reset_index()
-    store_pca_components_df = pca_components_df[(pca_components_df['SubChainName']==SubChainName) & (pca_components_df['StoreName']==StoreName)]
-    competitors_pca_components_df = pca_components_df[(pca_components_df['pca_birch_labels']==store_pca_components_df['pca_birch_labels'].iloc[0]) & (pca_components_df['SubChainName']!=store_pca_components_df['SubChainName'].iloc[0])]
+    store_pca_components_df = pca_components_df[(pca_components_df['StoreID']==Store_input_data['StoreID'].iloc[0])]
+    competitors_pca_components_df = pca_components_df[(pca_components_df['pca_birch_labels']==store_pca_components_df['pca_birch_labels'].iloc[0]) & (pca_components_df['SubChainID']!=store_pca_components_df['SubChainID'].iloc[0])]
 
-    
     # Calculate Euclidean distance for each row in competitors data
     store_row = store_pca_components_df.iloc[0][['P_M_1','P_M_2','P_L_1','P_L_2']]
     euclidean_distances = competitors_pca_components_df[['P_M_1','P_M_2','P_L_1','P_L_2']].apply(lambda row: euclidean_distance(store_row, row), axis=1)
     competitors_pca_components_df['EuclideanDistance'] = euclidean_distances
     competitors_pca_components_df = competitors_pca_components_df.sort_values(by=['EuclideanDistance'])
+    # Select top 5 competitors
     top5competitors = competitors_pca_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']][:5].reset_index(drop=True)
 
     return top5competitors

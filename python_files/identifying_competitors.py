34e03b3da62b2a2cb01da6e08f77e3def2df1153
isrkan <111886585+isrkan@users.noreply.github.com>
2023-05-28 07:39:37 +0200

    Adding geographic filters

--- python_files/identifying_competitors.py
+++ python_files/identifying_competitors.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import matplotlib.patches as mpatches
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.io as pio
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -14,13 +12,13 @@ from rpy2.robjects import pandas2ri
 readRDS = robjects.r['readRDS']
 
 
-def identify_competitors(category,product_description,SubChainName,StoreName):
+def identify_competitors(category,product_description,SubChainName,StoreName,Geographic=None):
 
     # Import the store data
-    Store_data = pd.read_csv('Store_data_git.csv')
-    Store_data = Store_data[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
+    Store_data = pd.read_csv('competitor_recognition_data/Store_data_git.csv')
+    Store_data = Store_data[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']]
     # Merge with Subchain names
-    Subchain_names = pd.read_excel('SubChainNameEnglish.xlsx')
+    Subchain_names = pd.read_excel('competitor_recognition_data/SubChainNameEnglish.xlsx')
     Store_data = Store_data.merge(Subchain_names, on=['SubChainID','SubChainName'], how='left')
     Store_data['SubChainName'] = Store_data['EnglishName']
     Store_data = Store_data.drop(columns=['EnglishName'])
@@ -28,8 +26,8 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     # Save the store and chain IDs
     Store_input_data = Store_data[(Store_data['SubChainName']==SubChainName) & (Store_data['StoreName']==StoreName)]
 
-    # Import the price data in one category and convert it to wide data
-    category_df = pd.read_parquet(str(category+'.parquet'))
+    # Import the price data in one category
+    category_df = pd.read_parquet('competitor_recognition_data/'+str(category+'.parquet'))
     category_df = category_df.set_index(['category','ProductDescription','StoreID'])
 
     # Filter observations with more than 25% nan values
@@ -47,7 +45,25 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     # Merge with store data
     product_df['StoreID'] = product_df['StoreID'].astype('int64')
     product_df = pd.merge(product_df, Store_data, how="left", left_on="StoreID", right_on="StoreID")
-    product_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','category','ProductDescription'], inplace=True)
+
+    # Filter by region
+    if Geographic=='City':
+        if len(product_df[product_df['CityName']==Store_input_data['CityName'].iloc[0]])>10:
+            product_df = product_df[product_df['CityName']==Store_input_data['CityName'].iloc[0]]
+        else:
+            Geographic=='County'
+
+    if Geographic=='County':
+        if len(product_df[product_df['SubDistrictName']==Store_input_data['SubDistrictName'].iloc[0]])>10:
+            product_df = product_df[product_df['SubDistrictName']==Store_input_data['SubDistrictName'].iloc[0]]
+        else:
+            Geographic=='District'
+
+    if Geographic=='District':
+        if len(product_df[product_df['DistrictName']==Store_input_data['DistrictName'].iloc[0]])>10:
+            product_df = product_df[product_df['DistrictName']==Store_input_data['DistrictName'].iloc[0]]
+
+    product_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName','category','ProductDescription'], inplace=True)
     
     ### Scale the data for price movement
     std_scaler = StandardScaler()
@@ -61,37 +77,31 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
 
     #### Dimensionality reduction #####
 
-    # Create a dictionary that allocates unique color to unique subschain
-    subchain_names = product_df.reset_index()['SubChainName'].unique()
-    color_map = dict(zip(subchain_names, cm.rainbow(np.linspace(0, 1, len(subchain_names)))))
-    # Create a list of patches (colored rectangles) for the legend
-    legend_patches = [mpatches.Patch(color=color, label=label) for label, color in color_map.items()]
-
     ###### PCA #####
     ### Price movement ###
     pca = PCA()
     price_movement_principal_components  = pca.fit_transform(price_movement_df)
     price_movement_principal_df = pd.DataFrame(data = price_movement_principal_components)
-    price_movement_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
+    price_movement_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']]
     price_movement_principal_pca = price_movement_principal_df.copy()
 
     ### Price level ###
     pca = PCA()
     price_level_principal_components  = pca.fit_transform(price_level_df)
     price_level_principal_df = pd.DataFrame(data = price_level_principal_components)
-    price_level_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
+    price_level_principal_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']]
     price_level_principal_pca = price_level_principal_df.copy()
 
     ### Combination - price level and Price movement ###
     combined_components_df = pd.concat([price_movement_principal_df.iloc[:, 0],price_level_principal_df.iloc[:, 0]], axis=1)
     combined_components_df.columns = ['PrincipalPriceMovement','PrincipalPricelevel']
-    combined_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
+    combined_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']]
 
 
     ## plot the principal components
-    fig = px.scatter(combined_components_df, x='PrincipalPriceMovement', y='PrincipalPricelevel', color='SubChainName', hover_data=['ChainName','SubChainName', 'StoreName'])
+    fig1 = px.scatter(combined_components_df, x='PrincipalPriceMovement', y='PrincipalPricelevel', color='SubChainName', hover_data=['ChainName','SubChainName', 'StoreName'])
     # Update the color of points matching the store
-    fig.add_trace(go.Scatter(
+    fig1.add_trace(go.Scatter(
         x=combined_components_df[combined_components_df['StoreID'] == Store_input_data['StoreID'].iloc[0]]['PrincipalPriceMovement'],
         y=combined_components_df[combined_components_df['StoreID'] == Store_input_data['StoreID'].iloc[0]]['PrincipalPricelevel'],
         mode='markers',
@@ -101,12 +111,13 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
         showlegend=False
     ))
     # Bold the input subchain
-    fig.for_each_trace(lambda t: t.update(name=t.name.replace(Store_input_data['SubChainName'].iloc[0], '<b>' + Store_input_data['SubChainName'].iloc[0] + '</b>')) if t.name == Store_input_data['SubChainName'].iloc[0] else t)
-    fig.update_layout(
-        title='Market Structure Analysis: Pricing Similarity in Terms of Price Level and Price Movement',
+    fig1.for_each_trace(lambda t: t.update(name=t.name.replace(Store_input_data['SubChainName'].iloc[0], '<b>' + Store_input_data['SubChainName'].iloc[0] + '</b>')) if t.name == Store_input_data['SubChainName'].iloc[0] else t)
+    fig1.update_layout(
+        title='Market Structure: Pricing Similarity in Terms of Price Level and Price Movement',
         legend=dict(title='Sub-Chain Name',yanchor="top",y=0.99,xanchor="left",x=1)
     )
-    fig.show()
+    #fig.show()
+    pio.write_html(fig1, 'static/market_structure_analysis.html')
 
     #### Cluster analysis #####
 
@@ -116,8 +127,8 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     pca_components_df = pd.concat([price_movement_principal_pca.iloc[:, 0:2],price_level_principal_pca.iloc[:, 0:2]], axis=1)
     pca_components_df.columns = ['P_M_1','P_M_2','P_L_1','P_L_2']
     # Add relevant columns
-    pca_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName']]
-    pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName'], inplace=True)
+    pca_components_df[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']] = product_df.reset_index()[['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName']]
+    pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName'], inplace=True)
 
     # Filter out stores from the same chain
     pca_components_df = pca_components_df.reset_index()
@@ -125,7 +136,7 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     same_chain_pca_components_df = pca_components_df[(pca_components_df['ChainID']==Store_input_data['ChainID'].iloc[0]) & (pca_components_df['StoreID']!=Store_input_data['StoreID'].iloc[0])]
     other_chains_pca_components_df = pca_components_df[pca_components_df['ChainID']!=Store_input_data['ChainID'].iloc[0]]
     pca_components_df = pd.concat([store_pca_components_df,other_chains_pca_components_df])
-    pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName'], inplace=True)
+    pca_components_df.set_index(['ChainID','ChainName','SubChainID','SubChainName','StoreID','StoreName','DistrictName','SubDistrictName','CityName'], inplace=True)
 
     # Define and fit the BIRCH model
     birch = Birch(threshold=9, n_clusters=None)
@@ -137,17 +148,19 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     pca_components_df = pca_components_df.reset_index()
     store_pca_components_df = pca_components_df[(pca_components_df['StoreID']==Store_input_data['StoreID'].iloc[0])]
     competitors_pca_components_df = pca_components_df[(pca_components_df['pca_birch_labels']==store_pca_components_df['pca_birch_labels'].iloc[0]) & (pca_components_df['SubChainID']!=store_pca_components_df['SubChainID'].iloc[0])]
+    if len(competitors_pca_components_df)==0:
+        competitors_pca_components_df = pca_components_df[pca_components_df['SubChainID']!=store_pca_components_df['SubChainID'].iloc[0]]
     same_chain_pca_components_df['pca_birch_labels'] = max(np.unique(pca_birch_labels))+1
     
     pca_components_df = pd.concat([pca_components_df,same_chain_pca_components_df])
     pca_birch_labels = np.concatenate((pca_birch_labels, same_chain_pca_components_df['pca_birch_labels'].to_numpy() ))
     
     # Plot BIRCH clusters on PCA components
-    fig = go.Figure()
+    fig2 = go.Figure()
 
     # Add points of all clusters
     cluster_mask = ((pca_components_df['pca_birch_labels'] != max(np.unique(pca_birch_labels))) & (pca_components_df['pca_birch_labels'] != store_pca_components_df['pca_birch_labels'].iloc[0]))
-    fig.add_trace(go.Scatter(
+    fig2.add_trace(go.Scatter(
         x=pca_components_df[cluster_mask]['P_M_1'],
         y=pca_components_df[cluster_mask]['P_L_1'],
         mode='markers',
@@ -166,7 +179,7 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
 
     # Add marks on stores from the same chain
     cluster_mask = pca_components_df['pca_birch_labels'] == max(np.unique(pca_birch_labels))
-    fig.add_trace(go.Scatter(
+    fig2.add_trace(go.Scatter(
         x=pca_components_df[cluster_mask]['P_M_1'],
         y=pca_components_df[cluster_mask]['P_L_1'],
         mode='markers',
@@ -181,7 +194,7 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
 
     # Add bolder points for the specific cluster
     cluster_mask = pca_components_df['pca_birch_labels'] == store_pca_components_df['pca_birch_labels'].iloc[0]
-    fig.add_trace(go.Scatter(
+    fig2.add_trace(go.Scatter(
         x=pca_components_df[cluster_mask]['P_M_1'],
         y=pca_components_df[cluster_mask]['P_L_1'],
         mode='markers',
@@ -195,7 +208,7 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
     ))
 
     # Add arrow annotation
-    fig.add_annotation(
+    fig2.add_annotation(
         x=store_pca_components_df['P_M_1'].iloc[0],
         y=store_pca_components_df['P_L_1'].iloc[0],
         ax=-60,
@@ -207,12 +220,13 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
         arrowcolor='black'
     )
 
-    fig.update_layout(
+    fig2.update_layout(
         xaxis_title='Principal Price Movement',
         yaxis_title='Principal Price Level',
         title='Store Clustering: Pricing Similarity using BIRCH Algorithm'
     )
-    fig.show()
+    #fig.show()
+    pio.write_html(fig2, 'static/store_clustering.html')
 
 
     # Calculate Euclidean distance
@@ -237,7 +251,8 @@ def identify_competitors(category,product_description,SubChainName,StoreName):
                 align='left'))
 
     layout = go.Layout(title='Top 5 Competitors')
-    fig = go.Figure(data=[table_trace], layout=layout)
-    fig.show()
+    fig3 = go.Figure(data=[table_trace], layout=layout)
+    #fig.show()
+    pio.write_html(fig3, 'static/top_competitors.html')
 
-    return top5competitors
+    return fig1, fig2, fig3, top5competitors

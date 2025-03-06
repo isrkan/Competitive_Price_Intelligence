0b95ed38068c3d89653e227f70b9bc7eead5ee94
isrkan <111886585+isrkan@users.noreply.github.com>
2025-03-06 15:59:46 +0100

    Add function to load store data

---
+++ ml_packages/competitor_identification/competitor_identification/data/load_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def load_store_data(store_file_path, subchain_file_path):
+    """
+    Loads store data from a CSV file and merges it with subchain names from an Excel file.
+
+    Parameters:
+    - store_file_path (str): Path to the store data CSV file.
+    - subchain_file_path (str): Path to the subchain names Excel file.
+
+    Returns:
+    - DataFrame: Merged store data with subchain English names.
+    """
+    # Load store data
+    store_data = pd.read_csv(store_file_path)
+    store_data = store_data[['ChainID', 'ChainName', 'SubChainID', 'SubChainName', 'StoreID', 'StoreName', 'DistrictName', 'SubDistrictName', 'CityName']]
+
+    # Load subchain names
+    subchain_names = pd.read_excel(subchain_file_path)
+
+    # Merge store data with subchain names based on SubChainID and SubChainName
+    store_data = store_data.merge(subchain_names[['SubChainID', 'SubChainName', 'EnglishName']], on=['SubChainID', 'SubChainName'], how='left')
+    # Replace SubChainName with the English name from the subchain_names file
+    store_data['SubChainName'] = store_data['EnglishName']
+    # Drop the EnglishName column as it is no longer needed
+    store_data = store_data.drop(columns=['EnglishName'])
+
+    return store_data
